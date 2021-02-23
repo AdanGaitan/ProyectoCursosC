a2a5421b5adf145cd490f178406a6bdc2e6704f8
natlius <lucianogaitan@live.com.ar>
2021-02-22 22:54:09 -0300

    con validacion

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -35,143 +37,138 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.txtNombre = new System.Windows.Forms.TextBox();
-            this.txtApellido = new System.Windows.Forms.TextBox();
-            this.txtDni = new System.Windows.Forms.TextBox();
             this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
             this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtDni = new System.Windows.Forms.TextBox();
             this.txtEmail = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(174, 139);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(269, 139);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 1;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 24);
+            this.label1.Location = new System.Drawing.Point(12, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nombre :";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(250, 23);
+            this.label2.Location = new System.Drawing.Point(266, 30);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(50, 13);
-            this.label2.TabIndex = 1;
+            this.label2.TabIndex = 3;
             this.label2.Text = "Apellido :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(28, 52);
+            this.label3.Location = new System.Drawing.Point(12, 65);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "DNI :";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Direccion :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(250, 55);
+            this.label4.Location = new System.Drawing.Point(12, 100);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Telefono :";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Dni:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(28, 90);
+            this.label5.Location = new System.Drawing.Point(266, 100);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(58, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Direccion :";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "E-mail :";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(250, 93);
+            this.label6.Location = new System.Drawing.Point(266, 65);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(38, 13);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Email :";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Telefono :";
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(84, 21);
+            this.txtNombre.Location = new System.Drawing.Point(73, 27);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(148, 20);
-            this.txtNombre.TabIndex = 6;
-            // 
-            // txtApellido
-            // 
-            this.txtApellido.Location = new System.Drawing.Point(319, 20);
-            this.txtApellido.Name = "txtApellido";
-            this.txtApellido.Size = new System.Drawing.Size(148, 20);
-            this.txtApellido.TabIndex = 7;
-            // 
-            // txtDni
-            // 
-            this.txtDni.Location = new System.Drawing.Point(84, 49);
-            this.txtDni.Name = "txtDni";
-            this.txtDni.Size = new System.Drawing.Size(148, 20);
-            this.txtDni.TabIndex = 8;
+            this.txtNombre.Size = new System.Drawing.Size(157, 20);
+            this.txtNombre.TabIndex = 8;
             // 
             // txtTelefono
             // 
-            this.txtTelefono.Location = new System.Drawing.Point(319, 52);
+            this.txtTelefono.Location = new System.Drawing.Point(322, 62);
             this.txtTelefono.Name = "txtTelefono";
-            this.txtTelefono.Size = new System.Drawing.Size(148, 20);
+            this.txtTelefono.Size = new System.Drawing.Size(140, 20);
             this.txtTelefono.TabIndex = 9;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(322, 27);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(140, 20);
+            this.txtApellido.TabIndex = 10;
             // 
             // txtDireccion
             // 
-            this.txtDireccion.Location = new System.Drawing.Point(84, 87);
+            this.txtDireccion.Location = new System.Drawing.Point(73, 62);
             this.txtDireccion.Name = "txtDireccion";
-            this.txtDireccion.Size = new System.Drawing.Size(148, 20);
-            this.txtDireccion.TabIndex = 10;
+            this.txtDireccion.Size = new System.Drawing.Size(157, 20);
+            this.txtDireccion.TabIndex = 11;
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(73, 97);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(157, 20);
+            this.txtDni.TabIndex = 12;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(319, 90);
+            this.txtEmail.Location = new System.Drawing.Point(322, 97);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(148, 20);
-            this.txtEmail.TabIndex = 11;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(188, 139);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 12;
-            this.button1.Text = "Aceptar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(286, 139);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "Cancelar";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.txtEmail.Size = new System.Drawing.Size(140, 20);
+            this.txtEmail.TabIndex = 13;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 201);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(484, 188);
             this.Controls.Add(this.txtEmail);
-            this.Controls.Add(this.txtDireccion);
-            this.Controls.Add(this.txtTelefono);
             this.Controls.Add(this.txtDni);
+            this.Controls.Add(this.txtDireccion);
             this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtTelefono);
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -179,6 +176,8 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAgregar);
             this.Name = "Form1";
             this.Text = "Nuevo Alumno";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -187,8 +186,11 @@
 
         }
 
+
         #endregion
 
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -196,13 +198,11 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtNombre;
-        private System.Windows.Forms.TextBox txtApellido;
-        private System.Windows.Forms.TextBox txtDni;
         private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtApellido;
         private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtDni;
         private System.Windows.Forms.TextBox txtEmail;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -21,9 +21,10 @@ namespace AdministradorCursos
         private void Form1_Load(object sender, EventArgs e)
         {
 
-           
-            
+            txtNombre.Focus();
 
+            
+            
             
             
 
@@ -31,24 +32,105 @@ namespace AdministradorCursos
         }
         private void Limpiar()
         {
-            txtApellido.Clear();
-            txtNombre.Clear();
-            txtEmail.Clear();
+            txtApellido.Text="";
+            txtNombre.Text="";
+            
             txtDireccion.Clear();
             txtTelefono.Clear();
+            
             txtDni.Clear();
+            txtEmail.Clear();
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var prueba = new CursosDB();
-            prueba.Alumno.Add(new Alumno() { apellido = txtApellido.Text, nombre = txtNombre.Text, telefono =Convert.ToInt32(txtTelefono.Text), direccion = txtDireccion.Text, dni=Convert.ToInt32(txtDni.Text),email=txtEmail.Text }); ;
-            prueba.SaveChanges();
-            Limpiar();
+
         }
 
         private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+        public bool validar()
+        {
+            if (txtNombre.Text == "")
+            {
+                MessageBox.Show("Debe Completar el nombre...");
+                txtNombre.Focus();
+                return false;
+            }
+            if(txtApellido.Text == "")
+            {
+                MessageBox.Show("Debe completar el apellido..");
+                txtApellido.Focus();
+                return false;
+            }
+            if (txtDireccion.Text == "")
+            {
+                MessageBox.Show("Debe completar la direccion...");
+                txtDireccion.Focus();
+                return false;
+            }
+            if (string.IsNullOrEmpty(txtTelefono.Text))
+            {
+                MessageBox.Show("Debe Completar el telefono");
+                txtTelefono.Focus();
+                return false;
+
+            }
+            else
+            {
+                try
+                {
+                    Int32.Parse(txtTelefono.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Coloque solo numeros. ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtTelefono.Focus();
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if((e.KeyChar >=32 && e.KeyChar<=47) ||(e.KeyChar >=58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solo letras", " alerta ", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void txtDireccion_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+            if (validar())
+            {
+                var prueba = new CursosDB();
+                prueba.Alumno.Add(new Alumno() { apellido = txtApellido.Text, nombre = txtNombre.Text, telefono = Convert.ToInt32(txtTelefono.Text), direccion = txtDireccion.Text, dni = Convert.ToInt32(txtDni.Text), email = txtEmail.Text });
+                prueba.SaveChanges();
+                Limpiar();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
         {
             Limpiar();
         }
